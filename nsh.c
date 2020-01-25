ebc4b26456ec292f3edb639866110185f974deba
Joshua Sukhdeo <joshsukhdeo@gmail.com>
2020-01-24 22:30:54 -0500

    fixed issue with ls in ssubdirs. Todo: >,<,|

--- nsh.c
+++ nsh.c
@@ -1,4 +1,5 @@
 #include "types.h"
+#include "fs.h"
 #include "stat.h"
 #include "user.h"
 #include "fcntl.h"
@@ -26,15 +27,16 @@ void strtok(char*str,char delim,char* tok, int* ptr){
 }
 
 void runCMD(char* buf){
-   
+
     char * args[10];
     int ptr = 0;
-    char arg1[100],arg2[100];
-    
+    char arg1[100] ="",arg2[100] = "";
+
     // Prefix a slash to specify the root directory
     //  when executing a command
-    arg1[0] = '/';
     
+    arg1[0] = '/';
+
     //Get first command token for arg1
     strtok(buf,' ',arg1+1,&ptr);
     
@@ -45,16 +47,16 @@ void runCMD(char* buf){
         arg2[ptr-curPtr] = buf[ptr];
         ptr = ptr+1;
     }
-    
+    arg2[ptr-curPtr] = '\0';
     //Replace ending \n with endOfString char
     if(arg1[strlen(arg1)-1]=='\n')
         arg1[strlen(arg1)-1] = '\0';
-    if(arg2[strlen(arg2)-1]=='\n')
-        arg2[strlen(arg2)-1] = '\0';
+    //if(arg2[strlen(arg2)-1]=='\n')
+    //    arg2[strlen(arg2)-1] = '\0';
 
 
-    //if is currently a test
-    if (arg1[0] == 't' && arg1[1] =='a' && arg1[2]=='c' && arg1[3] == 'o' && arg1[4] == '\0') {
+    //if is currently a test for pipe
+    if (strcmp(arg1,"/tacocat") == 0) {
         while(arg2[0] != '\0'){
             strtok(buf,' ',arg2,&ptr);
             if(arg2[0] != '\0' && arg2[0] != ' '){
@@ -64,25 +66,33 @@ void runCMD(char* buf){
     //else execute the command specified
     }else {
         args[0] = arg1;
-        args[1] = arg2;
-       // printf(1,"args[0]|%s|\n",args[0]);
+        //Provide a dot to ls instead of a blank param
+        // to avoid fstat issues in subdirs
+        if(strcmp(arg1,"/ls") == 0 && arg2[0] == '\0'){
         
+	    arg2[0] = '.';
+        arg2[1] = '\0';
+    }
+            args[1] = arg2;
+        //printf(1,"args[1]|%s|\n",args[1]);
+
         exec(args[0],args);
         printf(1,"exec failed\n");
     }
 }
 int main(int argc,char* argv[])
-{   //Print the initial greeting
+{
+    //Print the initial greeting
     char greeting[] = "Initializing new Shell (nsh)";
     printf(1,"%s\n", greeting);
  
     
     char buffer[100];
-    char quitCommand[100] = "quit";
-    
+    int nbuf = 100;
+    _Bool exitCommand = 0;
     printf(1,"@");
-    gets(buffer,100);
-    while(*buffer != *quitCommand){
+    gets(buffer,nbuf);
+    do{
         //rtrim \n 
         buffer[strlen(buffer)-1] = '\0';
        //Change dir on cd to the following string
@@ -95,8 +105,11 @@ int main(int argc,char* argv[])
                 runCMD(buffer);
         wait();
         printf(1,"@");
+        memset(buffer,0,nbuf);
         gets(buffer,100);
-    }
+        exitCommand = buffer[0] == 'e' && buffer[1] == 'x' && buffer[2] == 'i' && buffer[3] == 't';
+        exitCommand = exitCommand && (buffer[5] == '\0' || buffer[5] == '\n' || buffer[5] == ' ');
+    }while(exitCommand == 0);
  
     exit();
 }
