1bc4193f8eef50257e7244c4b494a6687cca540d
Joshua Sukhdeo <joshsukhdeo@gmail.com>
2020-01-31 15:28:20 -0500

    Fixed STDOUT and STDIN redirection operators

--- nsh.c
+++ nsh.c
@@ -30,55 +30,104 @@ void runCMD(char* buf){
 
     char * args[10];
     int ptr = 0;
-    char arg1[100] ="",arg2[100] = "";
+    char arg0[100] ="",arg1[100] = "";
 
     // Prefix a slash to specify the root directory
     //  when executing a command
     
-    arg1[0] = '/';
+    arg0[0] = '/';
 
-    //Get first command token for arg1
-    strtok(buf,' ',arg1+1,&ptr);
+    //Get first command token for arg0
+    strtok(buf,' ',arg0+1,&ptr);
     
-    //Set arg2 to the remaining string
+    //Skip whitespace
+    while(buf[ptr] == ' ')
+        ptr++;
+    
+    //Set arg1 to the remaining string
     // after the first space
     int curPtr = ptr;
     while(ptr < strlen(buf)){
-        arg2[ptr-curPtr] = buf[ptr];
+        arg1[ptr-curPtr] = buf[ptr];
         ptr = ptr+1;
     }
-    arg2[ptr-curPtr] = '\0';
+    arg1[ptr-curPtr] = '\0';
     //Replace ending \n with endOfString char
-    if(arg1[strlen(arg1)-1]=='\n')
-        arg1[strlen(arg1)-1] = '\0';
-    //if(arg2[strlen(arg2)-1]=='\n')
-    //    arg2[strlen(arg2)-1] = '\0';
+    if(arg0[strlen(arg0)-1]=='\n')
+        arg0[strlen(arg0)-1] = '\0';
+    //if(arg1[strlen(arg1)-1]=='\n')
+    //    arg1[strlen(arg1)-1] = '\0';
 
-
+    ptr = 0;
     //if is currently a test for pipe
-    if (strcmp(arg1,"/tacocat") == 0) {
-        while(arg2[0] != '\0'){
-            strtok(buf,' ',arg2,&ptr);
-            if(arg2[0] != '\0' && arg2[0] != ' '){
-                printf(1,"%s ",arg2);
+    if (strchr(buf,'>') != 0) {
+        char temp[100]="",file[100]="";
+        int fd;
+        
+        strtok(buf,'>',temp,&ptr);
+        if(buf[ptr] == '>')
+            ptr++;
+        while(buf[ptr] == ' ')
+            ptr++;
+        strtok(buf,'>',file,&ptr);
+        strcpy(buf,temp);
+        
+        if(fork() >= 0){
+            close(1);
+            if((fd = open(file,O_RDWR|O_CREATE)) < 0){
+                printf(2,"Error opening %s for R/W",file);
+                exit();
             }
+            dup(fd);
+            runCMD(buf);
         }
+        wait();
     //else execute the command specified
-    }else {
-        args[0] = arg1;
+    }else if (strchr(buf,'<') != 0) {
+        char temp[100]="",file[100]="";
+        int fd;
+        
+        strtok(buf,'<',temp,&ptr);
+        while(buf[ptr] == ' ')
+            ptr++;
+        strtok(buf,'<',file,&ptr);
+        strcpy(buf,temp);
+        if(fork() >= 0){
+            close(1);
+            if((fd = open(file,O_RDONLY)) < 0){
+                printf(2,"Error opening %s for READ",file);
+                exit();
+            }
+            dup(fd);
+            runCMD(buf);
+        }
+        wait();
+    } else {
+        args[0] = arg0;
         //Provide a dot to ls instead of a blank param
         // to avoid fstat issues in subdirs
-        if(strcmp(arg1,"/ls") == 0 && arg2[0] == '\0'){
+        if(strcmp(arg0,"/ls") == 0 && arg1[0] == '\0')
+            strcpy(arg1,".");
         
-	    arg2[0] = '.';
-        arg2[1] = '\0';
-    }
-            args[1] = arg2;
-        //printf(1,"args[1]|%s|\n",args[1]);
+        args[1] = arg1;
+        //printf(2,"args[1]|%s|\n",args[1]);
 
         exec(args[0],args);
-        printf(1,"exec failed\n");
+        printf(2,"exec failed\n");
+       
     }
+     exit();
+}
+
+int
+getcmd(char *buf, int nbuf)
+{
+  printf(2, "@ ");
+  memset(buf, 0, nbuf);
+  gets(buf, nbuf);
+  if(buf[0] == 0) // EOF
+    return -1;
+  return 0;
 }
 int main(int argc,char* argv[])
 {
@@ -86,12 +135,23 @@ int main(int argc,char* argv[])
     char greeting[] = "Initializing new Shell (nsh)";
     printf(1,"%s\n", greeting);
  
+    static char buffer[100]; 
     
-    char buffer[100];
-    int nbuf = 100;
+  /*  // ----- START FROM sh.c -----
+      int fd;
+
+    // Ensure that three file descriptors are open.
+    while((fd = open("console", O_RDWR)) >= 0){
+        if(fd >= 3){
+        close(fd);
+        break;
+        }
+    }
+    // ------ End FROM sh,c ------
+    */
+    
     _Bool exitCommand = 0;
-    printf(1,"@");
-    gets(buffer,nbuf);
+    getcmd(buffer,sizeof(buffer));
     do{
         //rtrim \n 
         buffer[strlen(buffer)-1] = '\0';
@@ -104,9 +164,8 @@ int main(int argc,char* argv[])
             if(fork()==0 && *buffer != 0)
                 runCMD(buffer);
         wait();
-        printf(1,"@");
-        memset(buffer,0,nbuf);
-        gets(buffer,100);
+        strcpy(buffer,0);
+        getcmd(buffer,sizeof(buffer));
         exitCommand = buffer[0] == 'e' && buffer[1] == 'x' && buffer[2] == 'i' && buffer[3] == 't';
         exitCommand = exitCommand && (buffer[5] == '\0' || buffer[5] == '\n' || buffer[5] == ' ');
     }while(exitCommand == 0);
